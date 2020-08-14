73c5db559019b698490621d66bbb812aec8e409d
Mike Wong <wongmike1122@gmail.com>
2020-08-14 14:38:04 +0800

    added start_accumulate, get_new_frame, get_accumulated_frames methods in Camera

--- camera/camera.py
+++ camera/camera.py
@@ -32,11 +32,10 @@ class Camera:
         self.fps = 0
         self.cap = cv2.VideoCapture(self.cam_ip)
         self.new_frame = None
+        self.lock = threading.Lock()
         self.accum_frames = queue.Queue(maxsize=self.num_votes)
         self._is_started = False
-        self._is_accumulating = False
-        self.is_accum_starting = False
-        self.is_accum_finished = False
+        self._accum_flag = False
 
     def set(self, propId, value):
         """ For setting a opencv VideoCapture property
@@ -45,15 +44,6 @@ class Camera:
         """
         self.cap.set(propId, value)
 
-    def start(self):
-        if self._is_started:
-            print('{} had already been started'.format(self.cam_ip))
-            return None
-
-        self._is_started = True
-        self.thread = threading.Thread(target=self.updating, args=())
-        self.thread.start()
-
     def updating(self):
         while self._is_started:
             succ, frame = self.cap.read()
@@ -61,32 +51,68 @@ class Camera:
                 self.new_frame = None
                 self.reconnecting()
                 continue
+            self.update_new_frame(frame)
+            self.accumulate(frame)
+
+    def update_new_frame(self, frame):
+        with self.lock:
             self.new_frame = frame
-            if self.is_accum_starting or self._is_accumulating:
-                if self.is_accum_starting:
-                    self.clear_accum_frames()
-                    self.is_accum_starting = False
-                    self._is_accumulating = True
-                self.accumulate(frame)
-                if self.accum_frames.full():
-                    self.is_accum_finished = True
-                    self._is_accumulating = False
 
     def accumulate(self, frame):
-        try:
-            self.accum_frames.put_nowait(frame)
-        except Full:
-            print('UNEXPECTED: try to put frame when accum_frames is full: {}'.format(self.cam_ip))
+        if self._accum_flag:
+            try:
+                self.accum_frames.put_nowait(frame)
+            except Full:
+                print('UNEXPECTED: try to put frame when accum_frames is full: {}'.format(self.cam_ip))
+
+            if self.accum_frames.full():
+                self._accum_flag = False
+
+    def start(self):
+        if self._is_started:
+            print('UNEXPECTED: camera had already started: {}'.format(self.cam_ip))
+            return None
+
+        self._is_started = True
+        self.thread = threading.Thread(target=self.updating, args=())
+        self.thread.start()
+
+    def start_accumulate(self):
+        """to start uptting frames into self.accum_frames"""
+        if self._accum_flag:
+            print('UNEXPECTED: started accumulating while previous accumulating process un-finished: {}'.format(self.cam_ip))
+        if not self.accum_frames.empty():
+            print('UNEXPECTED: started accumulating while previous accumulated frames un-used: {}'.format(self.cam_ip))
+            self.clear_accum_frames()
+        self._accum_flag = True
+
+    def get_accumulated_frames(self):
+        """to get all the accumulated frames only if its already full"""
+        accum_frames = None
+        if self.accum_frames.full():
+            accum_frames = []
+            while not self.accum_frames.empty():
+                try:
+                    accum_frames.append(self.accum_frames.get_nowait())
+                except Empty:
+                    print('UNEXPECTED: try to get frame when accum_frames is empty: {}'.format(self.cam_ip))
+            accum_frames = np.array(accum_frames)
+        return accum_frames
+
+    def get_new_frame(self):
+        with self.lock:
+            new_frame = self.new_frame
+        return new_frame
                 
     def reconnecting(self):
         # try to reconnect untill succ
-        print('{} cannot read frame, trying to reconnect...'.format(self.cam_ip))
+        print('UNEXPECTED: camera cannot read frame, trying to reconnect...: {}'.format(self.cam_ip))
         while self._is_started:
             self.cap = cv2.VideoCapture(self.cam_ip)
             time.sleep(5)
             succ, _ = self.cap.read()
             if succ:
-                print('{} is now reconnected!'.format(self.cam_ip))
+                print('Camera is now reconnected: {}'.format(self.cam_ip))
                 return True
             else:
                 continue
