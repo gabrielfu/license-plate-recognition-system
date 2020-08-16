e480edc85e5b8f270b8335925c3e9f396e3d5e89
Mike Wong <wongmike1122@gmail.com>
2020-08-16 23:07:34 +0800

    fixed indentation error and undefined var error

--- sender/sender.py
+++ sender/sender.py
@@ -12,21 +12,21 @@ def encode_json(m):
 
 class KafkaSender:
     def __init__(self, config):
-    self.topic = config['topic']
-    self.bootstrap_servers = config['bootstrap_servers']
-    self.max_stored_msg = config['max_stored_msg']
+        self.topic = config['topic']
+        self.bootstrap_servers = config['bootstrap_servers']
+        self.max_stored_msg = config['max_stored_msg']
 
-    self.messages = queue.Queue(maxsize=self.max_stored_msg)
-    self._is_started = False
+        self.messages = queue.Queue(maxsize=self.max_stored_msg)
+        self._is_started = False
 
-    while True:
-        try:
-            self.producer =  KafkaProducer(bootstrap_servers=self.bootstrap_servers, 
-                                           value_serializer=encode_json)
-            break
-        except:
-            logging.error('Failed to initialize Kafka producer, re-initializing...: bootstrap_servers {}'.format(self.bootstrap_servers))
-            continue
+        while True:
+            try:
+                self.producer =  KafkaProducer(bootstrap_servers=self.bootstrap_servers, 
+                                               value_serializer=encode_json)
+                break
+            except:
+                logging.error('Failed to initialize Kafka producer, re-initializing...: bootstrap_servers {}'.format(self.bootstrap_servers))
+                continue
 
     def start_kafka_streaming(self):
         if self._is_started:
@@ -45,7 +45,7 @@ class KafkaSender:
             try:
                 msg = self.messages.get_nowait()
             except queue.Empty:
-                self.logger.exception('Sender try to get message when messages queue is empty (unexpected)')
+                logging.exception('Sender try to get message when messages queue is empty (unexpected)')
                 continue
 
             date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
