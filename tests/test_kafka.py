66e99ee766843dc5c63aafd9d5bb0cd12282e75e
gabrielfu <hfu.gabriel@gmail.com>
2022-07-27 16:26:54 +0800

    update test kafka

--- tests/test_kafka.py
+++ tests/test_kafka.py
@@ -3,31 +3,32 @@ import sys
 sys.path.insert(0, os.getcwd())
 import time
 import logging
+import json
+import threading
 import numpy as np
+from kafka import KafkaConsumer
 
 from lprs.sender.sender import KafkaSender
 from lprs.utils.utils import read_yaml
 from lprs.logger import setup_logging
 
 
-def run():
+# Read configs
+logger_cfg = read_yaml('./config/logger.yaml')
+kafka_cfg = read_yaml('./config/kafka.yaml')
 
-    # Read configs
-    logger_cfg = read_yaml('./config/logger.yaml')
-    kafka_cfg = read_yaml('./config/kafka.yaml')
+# Setup logging handlers & initialize logger
+os.makedirs(logger_cfg['log_dir'], exist_ok=True)
+setup_logging(logger_cfg)
 
-    # Setup logging handlers & initialize logger
-    os.makedirs(logger_cfg['log_dir'], exist_ok=True)
-    setup_logging(logger_cfg)
-    logging.info('-------------------------------------------------------')
-    logging.info('Starting Application...')
+def produce():
 
     # Initialize kafka sender and start output streaming
     logging.info('Starting Kafka sender...')
     sender = KafkaSender(kafka_cfg)
     sender.start_kafka_streaming()
 
-    while True:
+    for _ in range(3):
         license_numbers = {
             '123.0.0.1': {
                 "plate_num": "AB1234",
@@ -42,14 +43,31 @@ def run():
         }
 
         # Output with Kafka
-        logging.info(f'LPR RESULT: {license_numbers}')
-        try:
-            sender.send(license_numbers)
-        except Exception:
-            logging.critical("Sender failed to send LPR results!")
+        sender.send(license_numbers)
 
-        time.sleep(4)
+        time.sleep(2)
 
+    return
+
+
+def consume():
+    consumer = KafkaConsumer(kafka_cfg["topic"],
+                             bootstrap_servers=[kafka_cfg["bootstrap_servers"]],
+                             value_deserializer=lambda m: json.loads(m.decode('ascii')))
+
+    for message in consumer:
+        logging.info(f"=> Received {message.value}")
+
+
+def run():
+    consumer = threading.Thread(target=consume, args=())
+    consumer.daemon = True
+    consumer.start()
+
+    producer = threading.Thread(target=produce, args=())
+    producer.daemon = True
+    producer.start()
+    producer.join()
 
 if __name__ == '__main__':
     run()
